c41a7691c66e5ffbac7f863279d4c79bf4eb2941
Linda ONeill <oneill.linda21@gmail.com>
2019-03-18 14:13:23 -0500

    create components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 import './App.css';
 
+const Api_Key = "28eb5b7aca2f69ea512c6e9a88625125";
+
 class App extends Component {
+
+  getWeather = async (e) => {
+
+    const city = e.target.elements.city.value;
+
+    const country = e.target.elements.country.value;
+
+    e.preventDefault();
+
+    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${Api_Key}');
+
+    const response = await api_call.json();
+
+    console.log(response);
+
+  }
+
   render() {
     return (
-      <div className="App">
-
+      <div>
+        <Titles />
+        <Form loadWeather={this.getWeather} />
+        <Weather />
       </div>
     );
   }
---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titles = (props) =>{
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <p>Check the weather in Chicago this week.</p>
+    </div>
+  )
+}
+
+export default Titles;
